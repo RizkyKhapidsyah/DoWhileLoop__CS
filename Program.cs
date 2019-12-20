03f46ce49c15a527a51e218d14edd0707b3e93f1
Rizky Khapidsyah <rizkyhafitsyah@gmail.com>
2019-12-20 13:13:55 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DoWhileLoop__CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X, A, B, C;
+            string D;
+
+            X = 1;
+
+            Console.Write("Masukkan Jumlah Perulangan : "); A = Convert.ToInt32(D = Console.ReadLine());
+            Console.Write("Masukkan Kelipatan         : "); B = Convert.ToInt32(D = Console.ReadLine());
+
+            Console.WriteLine();
+
+            do
+            {
+                Console.WriteLine(X);
+                X += B;
+            } while (X <= A);
+
+            C = A / B;
+
+            Console.WriteLine();
+            Console.Write("Jumlah Angka Diulang Sebanyak : {0}x", C);
+            Console.ReadLine();
+        }
+    }
+}
